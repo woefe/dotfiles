b840793eb6e8d8eeb3170f08f260191444eb0235
Wolfgang Popp <mail@wolfgang-popp.de>
2017-03-16 11:48:15 +0100

    awesomewm: top bar height

--- awesomewm/.config/awesome/rc.lua
+++ awesomewm/.config/awesome/rc.lua
@@ -291,7 +291,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytasklist= awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height=19 })
 
     systray = nil
     if s.index == 1 then
