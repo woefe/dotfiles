8ce06e03624645afdcfc3a464e42327709933f1e
Wolfgang Popp <mail@wolfgang-popp.de>
2019-02-15 16:00:48 +0100

    Update Readme and install script

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
-# Dotfiles
+<h1 align="center">⚫⚫⚫dotfiles⚫⚫⚫</h1>
 
-This repo holds some config files for my favorite GNU/Linux software.
+![laptop setup](https://i.imgur.com/g5fTXnu.png)
+Wallpaper based on [Low Poly Wolf by zelda-Freak91](https://zelda-freak91.deviantart.com/art/Low-Poly-Art-Wolf-537626838)
+
 
 ## How to use it
 
-* Clone this repo:
-```shell
-git clone --recursive https://github.com/woefe/dotfiles.git $HOME/.dotfiles
-cd $HOME/.dotfiles
-```
-* Install GNU Stow:
-```shell
-# On Arch Linux
-sudo pacman -S stow
-```
-* Uncomment the lines in install.sh of dotfiles you want to install
-* Execute `./install.sh`
+1. Clone this repo:
 
-# Screenshots
-Laptop
-![laptop setup](https://i.imgur.com/g5fTXnu.png)
-Desktop
-![desktop setup](https://imgur.com/e7NTLIr.png)
+    ```shell
+    git clone --recursive https://github.com/woefe/dotfiles.git $HOME/.dotfiles-woefe
+    cd $HOME/.dotfiles-woefe
+    ```
+2. Install GNU Stow:
 
-Wallpaper based on [Low Poly Wolf by zelda-Freak91](https://zelda-freak91.deviantart.com/art/Low-Poly-Art-Wolf-537626838)
+    ```shell
+    # On Arch Linux
+    sudo pacman -S stow
+    ```
+3. Uncomment the lines in [`install.sh`](./install.sh) of dotfiles you want to install
+4. Execute `./install.sh`.
+    Make sure that you run the script in the main dotfiles directory.
+    I.e change directory to `.dotfiles-woefe` first.
--- install.sh
+++ install.sh
@@ -1,49 +1,49 @@
 #!/usr/bin/env bash
 
-# Before executing this script make sure you have GNU Stow installed!!
-
+###################### Boilerplate and preparation ######################
 function check_prog() {
-if ! hash "$1" > /dev/null 2>&1; then
-    echo "Command not found: $1. Aborting..."
-    exit 1
-fi
+    if ! hash "$1" > /dev/null 2>&1; then
+        echo "Command not found: $1. Aborting..."
+        exit 1
+    fi
 }
 
 check_prog stow
 check_prog curl
 
 mkdir -p "$HOME/.config"
-
-#stow alacritty
-#stow --no-folding autostart
-#stow compton
-#stow dunst
-#stow environment
-#stow --no-folding fish
-#stow greenclip
-#stow i3
-#stow --no-folding moc
-#stow --no-folding ranger
-#stow redshift
-#stow rofi
-#stow scripts
-#stow termite
-#stow tmux
-#stow --no-folding vim; mkdir -p $HOME/.vim/{swapfiles,undodir}; curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#ln -s ~/.vim ~/.config/nvim; ln -s ~/.vimrc ~/.config/nvim/init.vim #Installs nvim config (important: stow vim first)
-#stow wallpaper
-#stow xorg
-#stow ytcc
-#stow zathura
-#stow zsh
+#########################################################################
 
 
-cat << EOF
 
-There are still things to do if you installed one of the following configs!
+############################# How to use it #############################
+#                                                                       #
+# Uncomment the lines of the configs you want to install below.         #
+# Then run this script from within the dotfiles directory.              #
+# E.g. `cd ~/.dotfiles; ./install.sh`                                   #
+#                                                                       #
+# To uninstall the config later, run stow -D in the dotfiles directory. #
+# E.g. `cd ~/.dotfiles; stow -D vim`                                    #
+#                                                                       #
+#########################################################################
 
-VIM:
-To complete the vim configuration open vim and execute :PlugInstall
-
-EOF
-
+#stow --target "$HOME"              alacritty
+#stow --target "$HOME" --no-folding autostart
+#stow --target "$HOME"              compton
+#stow --target "$HOME"              dunst
+#stow --target "$HOME"              environment
+#stow --target "$HOME" --no-folding fish
+#stow --target "$HOME"              greenclip
+#stow --target "$HOME"              i3
+#stow --target "$HOME" --no-folding moc
+#stow --target "$HOME" --no-folding ranger
+#stow --target "$HOME"              redshift
+#stow --target "$HOME"              rofi
+#stow --target "$HOME"              scripts
+#stow --target "$HOME"              termite
+#stow --target "$HOME"              tmux
+#stow --target "$HOME" --no-folding vim
+#stow --target "$HOME"              xorg
+#stow --target "$HOME"              ytcc
+#stow --target "$HOME"              zathura
+#stow --target "$HOME"              zsh
