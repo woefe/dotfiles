a033d3dd3bd9fd8d931c4605af1f3054e2e9c6cd
Wolfgang Popp <mail@wolfgang-popp.de>
2020-04-10 10:05:41 +0200

    Update wallpaper and monitor handling

--- scripts/.local/bin/monitor.sh
+++ scripts/.local/bin/monitor.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+LAPTOP="eDP-1"
+DOCK_LARGE="DP-2-2"
+DOCK_SMALL="DP-2-1"
+
 case "$1" in
-    duplicate )
-        PRIMARY="eDP-1"
-        MIRROR="$(xrandr -q | grep ' connected' | grep -v $PRIMARY | cut -d' ' -f1)"
-        xrandr --output "$PRIMARY" --primary --mode 1920x1080 --pos 0x0 --output "$MIRROR" --mode 1920x1080 --pos 0x0
+    mirror )
+        MIRROR="$(xrandr -q | grep ' connected' | grep -v "$LAPTOP" | cut -d' ' -f1)"
+        xrandr --output "$LAPTOP" --primary --mode 1920x1080 --pos 0x0 --output "$MIRROR" --mode 1920x1080 --pos 0x0
+        ;;
+    dock )
+        xrandr --output "$LAPTOP" --primary --mode 1920x1080 --pos 1080x1440 --rotate normal --output "$DOCK_SMALL" --mode 1920x1080 --pos 0x0 --rotate left --output "$DOCK_LARGE" --mode 2560x1440 --pos 1080x0 --rotate normal
         ;;
     above )
-        xrandr --output DP-1 --auto --above eDP-1 --output DP-2 --auto --above eDP-1
+        xrandr --output DP-1 --auto --above "$LAPTOP" --output DP-2 --auto --above "$LAPTOP"
         ;;
     reset )
-        xrandr --output eDP-1 --primary --mode 1920x1080 --output DP-1 --off --output DP-2 --off
+        xrandr --output "$LAPTOP" --primary --mode 1920x1080 --output DP-1 --off --output DP-2 --off --output "$DOCK_SMALL" --off --output "$DOCK_LARGE" --off
         ;;
 esac
+
+"$XDG_CONFIG_HOME/polybar/launch.sh"
