b28a54cd792323c1930a7f448010d0ecd0b87ce9
Wolfgang Popp <mail@wolfgang-popp.de>
2022-01-26 20:44:02 +0100

    script: update monitor layout

--- scripts/.local/bin/monitor.sh
+++ scripts/.local/bin/monitor.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 LAPTOP="eDP-1"
-DOCK_LARGE="DP-2-2"
-DOCK_SMALL="DP-2-1"
+DOCK_PORTRAIT="DP-2-2"
+DOCK_LANDSCAPE="DP-2-1"
 
 case "$1" in
     mirror )
@@ -10,13 +10,13 @@ case "$1" in
         xrandr --output "$LAPTOP" --primary --mode 1920x1080 --pos 0x0 --output "$MIRROR" --mode 1920x1080 --pos 0x0
         ;;
     dock )
-        xrandr --output "$LAPTOP" --primary --mode 1920x1080 --pos 1080x1440 --rotate normal --output "$DOCK_SMALL" --mode 1920x1080 --pos 0x0 --rotate left --output "$DOCK_LARGE" --mode 2560x1440 --pos 1080x0 --rotate normal
+        xrandr --output "$LAPTOP" --primary --mode 1920x1080 --pos 1440x1440 --rotate normal --output "$DOCK_PORTRAIT" --mode 2560x1440 --pos 0x0 --rotate left --output "$DOCK_LANDSCAPE" --mode 2560x1440 --pos 1440x0 --rotate normal
         ;;
     above )
         xrandr --output DP-1 --auto --above "$LAPTOP" --output DP-2 --auto --above "$LAPTOP"
         ;;
     reset )
-        xrandr --output "$LAPTOP" --primary --mode 1920x1080 --output DP-1 --off --output DP-2 --off --output "$DOCK_SMALL" --off --output "$DOCK_LARGE" --off
+        xrandr --output "$LAPTOP" --primary --mode 1920x1080 --output DP-1 --off --output DP-2 --off --output "$DOCK_LANDSCAPE" --off --output "$DOCK_PORTRAIT" --off
         ;;
 esac
 
