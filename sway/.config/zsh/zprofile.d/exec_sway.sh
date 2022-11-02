ed5846c9f954c41b7b29f8d81be624577112d698
Wolfgang Popp <mail@wolfgang-popp.de>
2022-11-02 22:44:05 +0100

    sway: workaround waybar escaping issues + small fixes

--- sway/.config/zsh/zprofile.d/exec_sway.sh
+++ sway/.config/zsh/zprofile.d/exec_sway.sh
@@ -3,5 +3,8 @@
     && export SSH_AUTH_SOCK \
     && export GRIM_DEFAULT_DIR="$HOME/pictures" \
     && export WLR_DRM_NO_MODIFIERS=1 \
+    && export MOZ_ENABLE_WAYLAND=1 \
+    && export QT_QPA_PLATFORM=wayland \
+    && export QT_WAYLAND_DISABLE_WINDOWDECORATION="1" \
     && mkdir -p "$XDG_DATA_HOME/sway" \
     && exec sway > "$XDG_DATA_HOME/sway/sway.log" 2> "$XDG_DATA_HOME/sway/sway.err.log"
