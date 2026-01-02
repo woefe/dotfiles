44578beb7875dada80667bf3d04b78ac3b9f1de8
Wolfgang Popp <mail@wolfgang-popp.de>
2026-01-02 23:07:57 +0100

    hyprland: fixes for 0.53

--- hyprland/.config/zsh/zprofile.d/exec_hypr.sh
+++ hyprland/.config/zsh/zprofile.d/exec_hypr.sh
@@ -1,4 +1,4 @@
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] \
     && export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh \
     && mkdir -p "$XDG_DATA_HOME/hyprland" \
-    && exec hyprland > "$XDG_DATA_HOME/hyprland/hyprland.log" 2> "$XDG_DATA_HOME/hyprland/hyprland.err.log"
+    && exec start-hyprland > "$XDG_DATA_HOME/hyprland/hyprland.log" 2> "$XDG_DATA_HOME/hyprland/hyprland.err.log"
